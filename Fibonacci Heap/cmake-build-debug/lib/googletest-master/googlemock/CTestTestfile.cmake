d1d2efb7aec4721700b42cb4557b13cae2df0290
Saginbaeff <43472617+Saginbaeff@users.noreply.github.com>
2018-11-25 17:32:20 +0300

    .

---
+++ Fibonacci Heap/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/C_Projects/Fibonacci Heap/lib/googletest-master/googlemock
+# Build directory: C:/C_Projects/Fibonacci Heap/cmake-build-debug/lib/googletest-master/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
